0a6b99a7bf5e09a66aac9854498abff5421be077
WildRC <valentim.ruben@gmail.com>
2019-01-01 14:20:51 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,16 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
 if($method == 'POST'){
-  $response = "test message";
-  echo json_encode($response);
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	$text = $json->result->parameters->text;
+
+	$response = new \stdClass();
+	$response->speech = $text;
+	$response->displayText = $text;
+	$response->source = "webhook";
+	echo json_encode($response);
 }
 else
 {
