344be47aea36943b7c4e76f60411c80a88ebf224
WildRC <valentim.ruben@gmail.com>
2019-01-01 14:10:53 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
 if($method == 'POST'){
-  $response = "test message"
-	echo json_encode($response);
+  $response = "test message";
+  echo json_encode($response);
 }
 else
 {
