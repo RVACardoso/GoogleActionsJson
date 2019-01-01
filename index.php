56e1142930be03c9c22702e758b52c4ccc5fcd6a
WildRC <valentim.ruben@gmail.com>
2019-01-01 14:10:17 +0000

    create

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if($method == 'POST'){
+  $response = "test message"
+	echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
+
+?>
