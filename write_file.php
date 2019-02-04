75536f6c4048fba492664eaae615d18a3a8ce1b4
WildRC <valentim.ruben@gmail.com>
2019-02-04 19:28:10 +0000

    Update write_file.php

--- write_file.php
+++ write_file.php
@@ -4,8 +4,9 @@
 if ($_POST["id"]==verysafe){
   $stored_data = file_get_contents('data.json');
   $json_data = json_decode($stored_data);
+  
   echo $_POST["name"];
-  $json_data->$_POST["name"] = $_POST["value"];
+  $json_data->humidade = $_POST["value"];
   $content = json_encode($json_data);
   file_put_contents("data.json", $content);
 }
