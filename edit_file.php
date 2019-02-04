a484cf570d822e17ad32606ebbc57dd5dcae18c7
WildRC <valentim.ruben@gmail.com>
2019-02-04 18:20:45 +0000

    Update edit_file.php

--- edit_file.php
+++ edit_file.php
@@ -1,8 +1,8 @@
 <?php
 
-  $filename = "data.json";
-  $content = "test content for json";
-  file_put_contents($filename, $content);
+//   $filename = "data.json";
+//   $content = "test content for json";
+//   file_put_contents($filename, $content);
   
   echo "contents: \n";
   echo file_get_contents('data.json');
