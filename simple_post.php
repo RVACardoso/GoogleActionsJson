1654db6d8484810064a059aafb527ffd7fecb69a
WildRC <valentim.ruben@gmail.com>
2019-02-04 17:59:13 +0000

    Update simple_post.php

--- simple_post.php
+++ simple_post.php
@@ -1,11 +1,28 @@
-<html>
-<body>
+<?php
 
-<form action="data_json.php" method="post">
-var1: <input type="text" name="var1"><br>
-var2: <input type="text" name="var2"><br>
-<input type="submit">
-</form>
+// where are we posting to?
+$url = 'https://wildrc-test1.herokuapp.com/data_json.php';
 
-</body>
-</html> 
+// what post fields?
+$fields = array(
+  'var1' => "testing_post"
+);
+
+// build the urlencoded data
+$postvars = http_build_query($fields);
+
+// open connection
+$ch = curl_init();
+
+// set the url, number of POST vars, POST data
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, count($fields));
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
+
+// execute post
+$result = curl_exec($ch);
+
+// close connection
+curl_close($ch);
+
+?>
