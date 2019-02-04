0fd756753abda8912b5609ca41845500694c951a
WildRC <valentim.ruben@gmail.com>
2019-02-04 19:10:08 +0000

    Create post_file.php

---
+++ post_file.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action="write_file.php" method="post">
+ID: <input type="text" name="id"><br>
+Name: <input type="text" name="name"><br>
+Value: <input type="text" name="value"><br>
+<input type="submit">
+</form>
+
+</body>
+</html> 
