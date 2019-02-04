e812ae79b66bdc30cb2786b60623e8052fe2facc
WildRC <valentim.ruben@gmail.com>
2019-02-04 17:47:24 +0000

    Create simple_post.php

---
+++ simple_post.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="data_json.php" method="post">
+var1: <input type="text" name="var1"><br>
+var2: <input type="text" name="var2"><br>
+<input type="submit">
+</form>
+
+</body>
+</html> 
