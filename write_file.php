1c23b5e4011770986467518220eec966b06d2141
WildRC <valentim.ruben@gmail.com>
2019-02-04 19:15:38 +0000

    Update write_file.php

--- write_file.php
+++ write_file.php
@@ -1,8 +1,15 @@
 
 <?php
 
-$stored_data = file_get_contents('data.json');
-$json_data = json_decode($stored_data);
-$json_stored = $json_data->$request;
-
+if ($_POST["id"]==verysafe){
+  $stored_data = file_get_contents('data.json');
+  $json_data = json_decode($stored_data);
+  $json_data->$_POST["name"] = $_POST["value"];
+  $content = json_encode($json_data);
+  file_put_contents("data.json", $content);
+}
+else
+{
+  echo "Wrong id";  
+}
 ?>
